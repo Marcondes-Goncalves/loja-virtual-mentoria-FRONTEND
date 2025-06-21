594313bfed3a68ea0993517d75b8557250c36768
Marcondes <marcondesgoncalves159@gmail.com>
2025-06-21 18:52:07 -0300

    direcionamento automático Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { usuario } from './model/usuario';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { LoginService } from './services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = '';
 
-  constructor (private fb: FormBuilder, private loginService: LoginService) {
+  constructor (private fb: FormBuilder, private loginService: LoginService, private router: Router) {
   
+  }
+
+  // tudo que estiver dentro do init é executado ao iniciar o sistema
+  ngOnInit(): void {
+
+    if (this.loginService.usuarioLogado()) {
+      this.router.navigate(['home'])
+    }else{
+      this.router.navigate(['login'])
+    }
+    
   }
 
 
