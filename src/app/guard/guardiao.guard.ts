ee67611edb8d1167410e566f8bcf072beedb641b
Marcondes <marcondesgoncalves159@gmail.com>
2025-06-22 16:10:25 -0300

    Guardiao finalizado

--- src/app/guard/guardiao.guard.ts
+++ src/app/guard/guardiao.guard.ts
@@ -3,7 +3,7 @@ import { CanActivateFn } from '@angular/router';
 import { LoginService } from '../services/login.service';
 import { environment } from 'src/environments/environments';
 
-// com o guardião verificamos se o usuário está logado para determinarmos a que rotas ele terá acesso no app.module
+// com o guardião verificamos se o usuário está logado e se possui acessos para determinarmos a que rotas ele terá acesso no app.module
 export const guardiaoGuard: CanActivateFn = (route, state) => {
 
   var username = localStorage.getItem('username');
@@ -12,14 +12,14 @@ export const guardiaoGuard: CanActivateFn = (route, state) => {
   var role = JSON.parse(JSON.stringify(roles)).role.toString();
   var authorization = '' + localStorage.getItem("Authorization");
 
-  // Faz uma requisição SINCRONA para verificar se o usuário possui os acessos necessários para acessar determinada tela
+  // Faz uma requisição SINCRONA para verificar quais acessos o usuário possui
   var request = new XMLHttpRequest();
   request.open('GET', environment.urlApi + 'possuiAcesso/' + username + '/' + role, false);
   request.setRequestHeader('Authorization', authorization);
   request.send();
-  
-  var possuiAcessoRetorno = request.responseText;
-  console.info("possuiAcessoRetorno: " + possuiAcessoRetorno);
 
-  return inject(LoginService).usuarioLogado();
+  var possuiAcessoRetorno = request.responseText === 'true' || new Boolean(request.responseText) === true;
+  var usuarioLogado = inject(LoginService).usuarioLogado();
+
+  return usuarioLogado  && possuiAcessoRetorno;
 };
